a79778b56e0e9e8c38789210674271d55332e1e2
tangyihang <820012136@qq.com>
2016-10-26 00:04:20 +0800

    7

---
+++ common/config/sphinxServer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * sphinx配置文件
+ */
+return [
+    'sphinxServer' => [
+        //直营城市非聚合售房
+        'bookinfo' => [
+            'port' => '9897',
+            'server' => '120.76.123.86',
+            'indexName' => 'bookinfo'
+        ],
+		'booksource' => [
+			'port' => '9898',
+			'server' => '120.76.123.86',
+			'indexName' => 'booksource'
+		],
+		'chapters' => [
+			'port' => '9903',
+			'server' => '120.76.123.86',
+			'indexName' => 'chapters'
+		],
+    ]
+];
