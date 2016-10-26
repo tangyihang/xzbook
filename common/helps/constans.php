cbdaf5319a123d05af43bfd3fbc367e11b01835d
tangyihang <820012136@qq.com>
2016-10-26 11:52:48 +0800

    1

--- common/helps/constans.php
+++ common/helps/constans.php
@@ -4,6 +4,7 @@ namespace common\helps;
 define('M_ROOT', substr(dirname(__FILE__), 0, -7));//根目录地址
 define('CODETABLEDIR', M_ROOT.'../encoding/');
 define('MCHARSET', 'utf-8');//系统页面默认字符集, 可选 'gbk', 'big5', 'utf-8'
+define('ONCEUPDATECHAPTER', 70);//一次刷新书籍个数
 define('PAGE_SIZE_TWELVE', 12); //分页个数
 define('PAGE_SIZE_HUNDRED', 100); //分页个数
 define('PAGE_SIZE_THOUSAND', 10000); //分页个数
